2b8a45c24833282ebcd833867f77fb142e70eac0
Dhruv Gupta <dhruv.sgit@gmail.com>
2023-04-02 10:38:20 +0530

    Test commit

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,6 +9,7 @@ public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
 
     public Calculator() {
+        //Hello world
     }
 
     public static void main(String[] args) {
